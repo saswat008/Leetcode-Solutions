2ad38998b6f40d2e6300e6d939a296c2b4b2588c
Saswat Kumar Pujari <saswat2603@gmail.com>
2024-11-19 14:33:11 +0530

    Create my-solution.java for Merge Sorted Array

---
+++ Top-Interview-150/1_Merge-Sorted-Array/my-solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = 0;
+        int j = 0;
+        int tempArr[] = new int[m+n];
+        int k = 0;
+        while(i<m && j<n) {
+            if(nums1[i]<=nums2[j]) {
+                tempArr[k++] = nums1[i++];
+            } else {
+                tempArr[k++] = nums2[j++];
+            }
+        }
+        while(i<m) {
+            tempArr[k++] = nums1[i++];
+        }
+        while(j<n) {
+            tempArr[k++] = nums2[j++];
+        }
+
+        for(int l=0; l<m+n; l++) {
+            nums1[l] = tempArr[l];
+        }
+    }
+}
